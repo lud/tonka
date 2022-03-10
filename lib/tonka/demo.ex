31327de4edc59e6b2363017561d1106607f00700
lud <ludovic@demblans.com>
2022-03-10 02:01:54 +0100

    working on demo script

--- lib/tonka/demo.ex
+++ lib/tonka/demo.ex
@@ -14,10 +14,16 @@ defmodule Tonka.Demo do
     # On init, the project will fill the container with services used by operations.
     container =
       Container.new()
-      |> Container.bind(Tonka.Service.Credentials, fn ->
-        File.cwd!()
-        |> Path.join("var/projects/dev/credentials.json")
-        |> Tonka.Service.Credentials.JsonFileCredentials.from_path!()
+      |> Container.bind(Tonka.Service.Credentials, fn c ->
+        store =
+          File.cwd!()
+          |> Path.join("var/projects/dev/credentials.json")
+          |> Tonka.Service.Credentials.JsonFileCredentials.from_path!()
+
+        {:ok, store, c}
       end)
+
+    {:ok, creds, container} = Container.pull(container, Tonka.Service.Credentials)
+    creds |> IO.inspect(label: "pulled", pretty: true)
   end
 end

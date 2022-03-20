5da7ac90da51314aaab7fd7146419e10ffd5569c
lud <ludovic@demblans.com>
2022-03-20 14:18:07 +0100

    new test

---
+++ test/grid_injection_test.exs
@@ -0,0 +1,10 @@
+defmodule Tonka.GridInjectionTest do
+  alias Tonka.Core.Grid
+  alias Tonka.Core.Action
+  alias Tonka.Core.Container
+  alias Tonka.Core.Grid.InvalidInputTypeError
+  alias Tonka.Core.Grid.NoInputCasterError
+  alias Tonka.Core.Grid.UnmappedInputError
+
+  use ExUnit.Case, async: true
+end

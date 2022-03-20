e3a79c27151e44a41ebb093310f44479d9e600d2
lud <ludovic@demblans.com>
2022-03-20 22:33:35 +0100

    removed return spec

--- lib/tonka/core/input_caster/nil_input.ex
+++
@@ -1,5 +0,0 @@
-defmodule Tonka.Core.InputCaster.NilInput do
-  def output_spec() do
-    %Tonka.Core.Container.ReturnSpec{type: {:type, {:atom, nil}}}
-  end
-end
--- test/support/fixtures/op_no_inputs.ex
+++ test/support/fixtures/op_no_inputs.ex
@@ -6,8 +6,5 @@ defmodule Tonka.Test.Fixtures.OpNoInputs do
   # def __mix_recompile__?, do: true
 
   @impl true
-  def output_spec, do: %Container.ReturnSpec{type: nil}
-
-  @impl true
   def call(_, _, _), do: {:ok, nil}
 end

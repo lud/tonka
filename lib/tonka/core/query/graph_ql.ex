061b71bcb94602f75b82267e7612faf567ace0b5
lud <ludovic@demblans.com>
2022-03-23 00:14:19 +0100

    starting graphql renderer

---
+++ lib/tonka/core/query/graph_ql.ex
@@ -0,0 +1,78 @@
+defmodule Tonka.Core.Query.GraphQL do
+  @format_schema NimbleOptions.new!(
+                   pretty: [type: :boolean, default: false],
+                   sort: [type: :boolean, default: false]
+                 )
+
+  require Record
+  Record.defrecordp(:c, indent: 0, pretty: false, sort: false)
+
+  def format_query(query, opts) when is_list(query) do
+    ctx =
+      opts
+      |> NimbleOptions.validate!(@format_schema)
+      |> base_context()
+
+    [open_query(ctx), open_brace(ctx), format_subs(query, ctx), close_brace(ctx)]
+    |> IO.inspect(label: "tokens")
+    |> :erlang.iolist_to_binary()
+    |> tap(&IO.puts/1)
+  end
+
+  defp base_context(opts) do
+    c(pretty: !!opts[:pretty], sort: !!opts[:sort], indent: 0)
+  end
+
+  defp open_query(_ctx), do: "query"
+
+  defp open_brace(c(pretty: true) = ctx), do: [" {", lf(ctx)]
+  defp open_brace(c(pretty: false) = ctx), do: ["{", lf(ctx)]
+  defp close_brace(c(pretty: true, indent: 0) = ctx), do: [indent(ctx), "}"]
+  defp close_brace(c(pretty: true) = ctx), do: [indent(ctx), "}", lf(ctx)]
+  defp close_brace(c(pretty: false) = ctx), do: "}"
+
+  defp indent(c(pretty: true, indent: n)), do: s_indent(n)
+  defp indent(c(pretty: false)), do: ""
+
+  defp s_indent(8), do: "                "
+  defp s_indent(7), do: "              "
+  defp s_indent(6), do: "            "
+  defp s_indent(5), do: "          "
+  defp s_indent(4), do: "        "
+  defp s_indent(3), do: "      "
+  defp s_indent(2), do: "    "
+  defp s_indent(1), do: "  "
+  defp s_indent(0), do: ""
+  defp s_indent(n), do: String.duplicate("  ", n)
+
+  defp lf(c(pretty: true)), do: ?\n
+  defp lf(c(pretty: false)), do: 32
+
+  defp indent_left(c(indent: n) = ctx) do
+    c(ctx, indent: n + 1)
+  end
+
+  defp indent_right(c(indent: n) = ctx) do
+    c(ctx, indent: n - 1)
+  end
+
+  defp format_subs(list, ctx) do
+    ctx = indent_left(ctx)
+
+    Enum.map(list, fn
+      bin when is_binary(bin) ->
+        [indent(ctx), format_field(bin), lf(ctx)]
+
+      {field, subs} ->
+        [
+          indent(ctx),
+          format_field(field),
+          open_brace(ctx),
+          format_subs(subs, ctx),
+          close_brace(ctx)
+        ]
+    end)
+  end
+
+  defp format_field(name), do: name
+end
---
+++ test/graphql_test.exs
@@ -0,0 +1,153 @@
+defmodule Tonka.GraphQLTest do
+  use ExUnit.Case, async: true
+  alias Tonka.Core.Query.GraphQL
+
+  test "basic queries" do
+    expected =
+      """
+      query {
+        a {
+          b
+        }
+      }
+      """
+      |> String.trim()
+
+    assert format([{"a", ["b"]}], pretty: true) ===
+             expected
+  end
+
+  @tag :skip
+  test "basic queries 2" do
+    expected =
+      """
+      query {
+        a {
+          b
+          c
+        }
+      }
+      """
+      |> String.trim()
+
+    assert format(%{a: ["b", :c]}, pretty: true) === expected
+  end
+
+  @tag :skip
+  test "basic queries 3" do
+    expected =
+      """
+      query {
+        a(sort: desc) {
+          b
+          c
+        }
+      }
+      """
+      |> String.trim()
+
+    assert format(%{{:a, sort: :desc} => ["b", :c]}, pretty: true) === expected
+  end
+
+  @tag :skip
+  test "empty args or bodies" do
+    assert format(%{{:a, nil} => ["b", :c]}) === "query { a { b c } }"
+    assert format(%{{:a, []} => ["b", :c]}) === "query { a { b c } }"
+    assert format(%{{:a, %{}} => ["b", :c]}) === "query { a { b c } }"
+    assert format(%{:a => []}) === "query { a }"
+  end
+
+  @tag :skip
+  test "list with tupes" do
+    assert format(%{:a => ["b", {:c, ["d", "e"]}]}) ===
+             "query { a { b c { d e } } }"
+
+    assert format(%{:a => ["b", {{:c, x: 1}, ["d", "e"]}]}) ===
+             "query { a { b c(x: 1) { d e } } }"
+  end
+
+  @tag :skip
+  test "strange behaviour" do
+    expected = "query { a { b { c } } }"
+    assert expected === format(%{"a" => %{"b" => "c"}})
+    assert expected === format(%{"a" => {"b", "c"}})
+    assert expected === format(%{"a" => [{"b", "c"}]})
+  end
+
+  defp format(query, opts \\ []) do
+    _v = GraphQL.format_query(query, opts)
+  end
+
+  @tag :skip
+  test "can format a query" do
+    query = %{
+      # tuple-keys allow to put arguments. Map body allows to have children
+      # query objects
+      {:project, fullPath: "company-agilap/r-d/agislack"} => %{
+        # atom values will not be quoted
+        {:issues, sort: :updated_desc} => %{
+          # simple keys will not have arguments
+          # lists can be used if no child has children or arguments.
+          # atom or string names will never be quoted
+          pageInfo: ["endCursor", :startCursor, "hasNextPage"],
+          # If mixing with leaves and sub-objects, an empty list can be used to
+          # mark a child a a leaf.
+          edges: %{
+            node: %{
+              :title => [],
+              :id => [],
+              :timeEstimate => [],
+              :webUrl => [],
+              :userNotesCount => [],
+              {:notes, first: 1} => %{
+                edges: %{
+                  node: %{id: [], body: [], system: [], author: ["username"]}
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    expected = """
+    query {
+      project(fullPath: "company-agilap/r-d/agislack") {
+        issues(sort: updated_desc) {
+          edges {
+            node {
+              id
+              notes(first: 1) {
+                edges {
+                  node {
+                    author {
+                      username
+                    }
+                    body
+                    id
+                    system
+                  }
+                }
+              }
+              timeEstimate
+              title
+              userNotesCount
+              webUrl
+            }
+          }
+          pageInfo {
+            endCursor
+            hasNextPage
+            startCursor
+          }
+        }
+      }
+    }
+    """
+
+    generated = format(query, pretty: true, sort: true)
+
+    assert String.trim(generated) ===
+             String.trim(expected)
+  end
+end

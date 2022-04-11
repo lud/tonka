c83274e87a5d25802dc2fbb55456ab5070e324a8
lud <ludovic@demblans.com>
2022-04-12 01:40:15 +0200

    extract job module

---
+++ lib/tonka/project/job.ex
@@ -0,0 +1,36 @@
+defmodule Tonka.Project.Job do
+  use GenServer, restart: :temporary, shutdown: 60_000
+  alias Tonka.Project
+  alias Tonka.Core.Grid
+
+  def start_link(opts) do
+    prk = Keyword.fetch!(opts, :prk)
+    pubkey = Keyword.fetch!(opts, :publication)
+    input = Keyword.fetch!(opts, :input)
+    GenServer.start_link(__MODULE__, %{prk: prk, pub: pubkey, input: input}, [])
+  end
+
+  @impl true
+  def init(%{prk: prk, pub: pubkey, input: input} = state) do
+    {:ok, state, {:continue, :start}}
+  end
+
+  def handle_continue(:start, %{prk: prk, pub: pubkey, input: input} = state) do
+    with {:ok, pub} <- Project.fetch_publication(prk, pubkey),
+         {:ok, container} <- Project.fetch_container(prk),
+         :ok <- run_grid(pub, container, input) do
+      {:stop, :normal, state}
+    else
+      {:error, reason} -> {:stop, reason, state}
+    end
+  end
+
+  defp run_grid(pub, container, input) do
+    grid = pub.grid
+
+    case Grid.run(grid, container, input) do
+      {:ok, :done, _grid} -> :ok
+      {:error, detail, _grid} -> {:error, detail}
+    end
+  end
+end

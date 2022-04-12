ab8cb64816cd2b1a65e3ae31802c9467973594b0
lud <ludovic@demblans.com>
2022-04-12 02:06:13 +0200

    0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tonka.MixProject do
   def project do
     [
       app: :tonka,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
